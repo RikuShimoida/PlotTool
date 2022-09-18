2aa5ee8fd92262b8e552f6b2312760aaf499e436
Riku <xiajingtianlu@gmail.com>
2022-09-19 01:07:37 +0900

    初回コミット

---
+++ src/main/java/plot_tool/user/controller/IndexController.java
@@ -0,0 +1,16 @@
+package plot_tool.user.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+
+@Controller
+public class IndexController {
+
+    @RequestMapping
+    public  void index() {
+
+    }
+
+
+}
